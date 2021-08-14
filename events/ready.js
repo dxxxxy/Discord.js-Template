276ea9f449e096862998c2d8bf10371d5c766509
DxxxxY <introsdream@gmail.com>
2021-08-14 13:36:38 -0400

    initial commit

---
+++ events/ready.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose")
+
+module.exports = (client) => { //required in every file
+    //mongodb connection (optional)
+    // mongoose.connect(process.env.DATABASE, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
+    // mongoose.connection.on('connected', () => {
+    //     console.log('Mongoose connection successfully opened!')
+    // })
+
+    // mongoose.connection.on('err', err => {
+    //     console.error(`Mongoose connection error: \n ${err.stack}`)
+    // })
+
+    // mongoose.connection.on('disconnected', () => {
+    //     console.log('Mongoose connection disconnected')
+    // })
+    console.log(`Logged in as ${client.user.tag}!`)
+}
